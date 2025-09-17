f3dc213e7bb19d2ac60530d78f8ae5ca5feb02a5
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-17 19:06:49 +0530

    Create TowerOfHanoi.c

---
+++ Sept_17/TowerOfHanoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void TowerOfHanoi(int n, int a, int b, int c) {
+    if(n>0) {
+        TowerOfHanoi(n-1, a, c, b);
+        printf("Move a disc from %d to %d\n", a, c);
+        TowerOfHanoi(n-1, b, a, c);
+    }
+}
+
+int main() {   
+    TowerOfHanoi(3, 1, 2, 3);
+    return 0;
+}
+
