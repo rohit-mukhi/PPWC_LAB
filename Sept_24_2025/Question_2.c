b56a0856dbe2f66b3c63c75a27d3720c408f0283
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-25 14:31:06 +0530

    Update Question_2.c

--- Sept_24_2025/Question_2.c
+++ Sept_24_2025/Question_2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 void check(char ch) {
-        if((ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') && (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')) {
+        if((ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') && (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')) {
             printf("%c is a vowel\n", ch);
             return;
-        } else if((ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') && (ch != 'a' || ch != 'e' || ch != 'i' || ch != 'o' || ch != 'u')) {
+        } else if((ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') && (ch != 'a' || ch != 'e' || ch != 'i' || ch != 'o' || ch != 'u' || ch != 'A' || ch != 'E' || ch != 'I' || ch != 'O' || ch != 'U')) {
             printf("%c is a consonant\n", ch);
             return;
         } else if(ch >= '0' && ch<='9') {
