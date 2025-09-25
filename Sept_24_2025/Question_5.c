81015a4a728297052f82b6007e73d2899864b13d
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-25 15:09:51 +0530

    Create Question_5.c

---
+++ Sept_24_2025/Question_5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int reverse(int num, int rev) {
+    if(num == 0) {
+        return rev;
+    } else {
+        return reverse(num/10, rev*10+num%10);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    char a = getchar();
+    int res = reverse(num, 0);
+    printf("The reversed num is: %d\n", res);
+    return 0;
+}
