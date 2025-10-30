b66e031acf24d0697f92541a47e26a5f88eaa12e
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-30 21:20:08 +0530

    Update Question_3.c

--- Minor_Assignment_3/Question_3.c
+++ Minor_Assignment_3/Question_3.c
@@ -13,7 +13,7 @@ void bubbleSort(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {2, 4, 5, 9, 7, 1, 3, 6};
+    int arr[] = {2, 4, 5, 9, 7, 1, 3, 6, 8};
     bubbleSort(arr, sizeof(arr)/sizeof(arr[0]));
     for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++) {
         printf("%d ", arr[i]);
