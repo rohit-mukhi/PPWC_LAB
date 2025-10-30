7c246efd6b0e3be83fbac67aba68dab48266b76e
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-30 21:07:45 +0530

    Update Question_2.c

--- Minor_Assignment_3/Question_2.c
+++ Minor_Assignment_3/Question_2.c
@@ -1,5 +1,7 @@
 void sumarr(int a[], int b[], int r[], int size);
 
+//main function goes here
+
 void sumarr(int a[], int b[], int r[], int size) {
     for(int i=0; i<size; i++) {
         r[i] = a[i] + b[i];
